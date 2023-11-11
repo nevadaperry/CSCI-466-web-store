44d64b5a29979760c6c91f24850b5dc30b9ee67c
Nevada Perry <nevadaperry@gmail.com>
2023-11-11 00:22:08 -0600

    Remove nonsense

--- backend/router.php
+++ backend/router.php
@@ -3,15 +3,15 @@
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
-switch ($method . $uri) {
+switch ($method . ' ' . $uri) {
 case 'GET /':
-	return 'Here is a response'
+	print 'Here is a response';
 	break;
 case 'GET /smoothie':
-	return 'Here is a smoothie'
+	print 'Here is a smoothie';
 	break;
 case 'PUT /smoothie':
-	return 'You put a smoothie'
+	print 'You put a smoothie';
 	break;
 }
 
