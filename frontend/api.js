bbeb1a8be1929d865c1c33ae35ee9ab2600b39b9
Nevada Perry <nevadaperry@gmail.com>
2023-12-04 22:07:03 -0600

    Fix frontend insertion after loading

--- frontend/api.js
+++ frontend/api.js
@@ -16,3 +16,26 @@ const api = {
 		url: `${apiUrl}/orders`,
 	})).data,
 };
+
+// https://stackoverflow.com/a/62475924/15295209
+window.onunhandledrejection = event => {
+	document.insertAdjacentHTML(
+		'afterbegin',
+		`Unhandled Promise rejection: ${event.reason}`,
+	);
+};
+window.onerror = (message, source, lineNumber, colno, error) => {
+	document.insertAdjacentHTML(
+		'afterbegin',
+		`Unhandled error: ${error.stack}`,
+	);
+};
+
+function removePlaceholders() {
+	const placeholders = document.getElementsByClassName(
+		'loading-placeholder',
+	);
+	for (const placeholder of placeholders) {
+		placeholder.classList.add('done-loading');
+	}
+}
