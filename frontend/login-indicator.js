db9a61cca287a437f1b4769f17479cf76834c9ec
Nevada Perry <nevadaperry@gmail.com>
2023-12-06 03:03:15 -0600

    Additional aesthetic improvements

--- frontend/login-indicator.js
+++ frontend/login-indicator.js
@@ -2,14 +2,14 @@ const email = localStorage.getItem('email');
 const loginStatus = document.getElementById('login-status');
 if (email === null) {
 	loginStatus.innerHTML = `
-		<span class="unbreak">
+		<span class="align-top pad-heavy">
 			Not logged in.
 		</span>
 		<span><a href="login.html">Log in</a></span>
 	`;
 } else {
 	loginStatus.innerHTML = `
-		<span class="unbreak">
+		<span class="align-top pad-heavy">
 			Logged in as <span class="white-gold">${email}</span>
 		</span>
 		<span><a href="#" onclick="logOut()">Log out</a></span>
