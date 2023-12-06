36fbf5657f36c43f8adac3c3ca30139288412bed
Nevada Perry <nevadaperry@gmail.com>
2023-12-05 23:00:16 -0600

    Add debug logging to POST /orders

--- backend/router.php
+++ backend/router.php
@@ -30,7 +30,8 @@ else if ("{$method} {$uri}" == 'GET /orders') {
 }
 else if ("{$method} {$uri}" == 'POST /orders') {
 	$order = file_get_contents('php://input');
-	print json_encode(post_order(json_decode($order)));
+	print gettype($order) . 'hello' . $order;
+	//print json_encode(post_order(json_decode($order)));
 }
 else if ("{$method} {$uri}" == 'GET /smoothie') {
 	print 'Here is a smoothie';
