dfe4d50aa1cfe57d937cfdd65c3b4d5db1c9952a
Aaron Hall <rsaaronh11@gmail.com>
2023-12-08 22:54:27 -0600

    added products

--- db/initial-data.sql
+++ db/initial-data.sql
@@ -1,5 +1,3 @@
-/* PRODUCTS */
-
 INSERT INTO product (name, description, price, stock)
 VALUES (
 	'MaryRuth Organics, Herbal Supplement Drop, Calmness & Relaxation, Pack Of 1, USDA Kava Root, Sugar/Gluten Free, Support Sleep, Calm & Stress Relief, Vegan, Non-GMO, 30 Servings',
@@ -49,203 +47,94 @@ VALUES (
 	129.00,
 	25
 );
-
-/* CUSTOMERS */
-
-INSERT INTO customer (email)
-VALUES ('jkinny4@starnet.net');
-INSERT INTO customer (email)
-VALUES ('procurement@gfs.com');
-INSERT INTO customer (email)
-VALUES ('where.are.my.keys@outlook.com');
-INSERT INTO customer (email)
-VALUES ('alfonso.ribeiro@belair.ca.gov');
-INSERT INTO customer (email)
-VALUES ('touch.the.sky7@gmail.com');
-
-/* ORDERS */
-
-INSERT INTO `order` (
-	customer_id,
-	shipping_address,
-	name_on_card,
-	card_number,
-	card_exp,
-	card_cvv,
-	card_zipcode,
-	phone_number
-)
-SELECT
-	c.id,
-	'123 Rainbow Road, Bend, OR 97003',
-	'Jennifer Kinny',
-	'4111111111111111',
-	'08/28',
-	'998',
-	'97003',
-	'800-555-5555'
-FROM customer c
-WHERE c.email = 'jkinny4@starnet.net';
-INSERT INTO order_line_item (
-	order_id,
-	product_id,
-	frozen_price,
-	quantity
-)
-SELECT
-	o.id,
-	p.id,
-	p.price,
-	1
-FROM `order` o
-JOIN customer c ON o.customer_id = c.id
-JOIN product p ON p.name = 'Louis Vuitton Pre-Loved Monogram Canvas Montaigne MM, Brown'
-WHERE c.email = 'jkinny4@starnet.net';
-
-INSERT INTO `order` (
-	customer_id,
-	shipping_address,
-	name_on_card,
-	card_number,
-	card_exp,
-	card_cvv,
-	card_zipcode,
-	phone_number
-)
-SELECT
-	c.id,
-	'123 Warehouse Rd, Wichita, KS 57331',
-	'Myra Westinghouse',
-	'4111111111111111',
-	'08/28',
-	'998',
-	'57331',
-	'800-555-5555'
-FROM customer c
-WHERE c.email = 'procurement@gfs.com';
-INSERT INTO order_line_item (
-	order_id,
-	product_id,
-	frozen_price,
-	quantity
-)
-SELECT
-	o.id,
-	p.id,
-	p.price,
-	1
-FROM `order` o
-JOIN customer c ON o.customer_id = c.id
-JOIN product p ON p.name like 'DEWALT 20V MAX Cordless Drill and Impact Driver%'
-WHERE c.email = 'procurement@gfs.com';
-
-INSERT INTO `order` (
-	customer_id,
-	shipping_address,
-	name_on_card,
-	card_number,
-	card_exp,
-	card_cvv,
-	card_zipcode,
-	phone_number
-)
-SELECT
-	c.id,
-	'332 Lampshade Pkwy, Jacksonville, FL 23345',
-	'Sustainable Living',
-	'4111111111111111',
-	'08/28',
-	'998',
-	'23345',
-	'800-555-5555'
-FROM customer c
-WHERE c.email = 'where.are.my.keys@outlook.com';
-INSERT INTO order_line_item (
-	order_id,
-	product_id,
-	frozen_price,
-	quantity
-)
-SELECT
-	o.id,
-	p.id,
-	p.price,
-	1
-FROM `order` o
-JOIN customer c ON o.customer_id = c.id
-JOIN product p ON p.name like 'Furhaven ThermaNAP Self-Warming Cat Bed%'
-WHERE c.email = 'where.are.my.keys@outlook.com';
-
-INSERT INTO `order` (
-	customer_id,
-	shipping_address,
-	name_on_card,
-	card_number,
-	card_exp,
-	card_cvv,
-	card_zipcode,
-	phone_number
-)
-SELECT
-	c.id,
-	'17 Winding Hill Dr, Bel Air, CA 90241',
-	'Alfonso Ribeiro',
-	'4111111111111111',
-	'08/28',
-	'998',
-	'90241',
-	'800-555-5555'
-FROM customer c
-WHERE c.email = 'alfonso.ribeiro@belair.ca.gov';
-INSERT INTO order_line_item (
-	order_id,
-	product_id,
-	frozen_price,
-	quantity
-)
-SELECT
-	o.id,
-	p.id,
-	p.price,
-	1
-FROM `order` o
-JOIN customer c ON o.customer_id = c.id
-JOIN product p ON p.name like 'ShelterLogic SuperMax All Purpose Outdoor 12 x 26-Feet Canopy Replacement Cover%'
-WHERE c.email = 'alfonso.ribeiro@belair.ca.gov';
-
-INSERT INTO `order` (
-	customer_id,
-	shipping_address,
-	name_on_card,
-	card_number,
-	card_exp,
-	card_cvv,
-	card_zipcode,
-	phone_number
-)
-SELECT
-	c.id,
-	'117 Eagle Circle, Terra Haute, IN 44271',
-	'Maureen Wall',
-	'4111111111111111',
-	'08/28',
-	'998',
-	'44271',
-	'800-555-5555'
-FROM customer c
-WHERE c.email = 'touch.the.sky7@gmail.com';
-INSERT INTO order_line_item (
-	order_id,
-	product_id,
-	frozen_price,
-	quantity
-)
-SELECT
-	o.id,
-	p.id,
-	p.price,
-	1
-FROM `order` o
-JOIN customer c ON o.customer_id = c.id
-JOIN product p ON p.name like 'Niceday Elliptical Machine, Elliptical Trainer%'
-WHERE c.email = 'touch.the.sky7@gmail.com';
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Slim Jim Giant Smoked Meat Snack Stick, Hot AF Flavor, 0.97-oz. Stick 24-Count",
+	"When it comes to snacking, they say size matters. That’s why Slim Jim Giant Hot AF Flavor Smoked Meat Sticks have big, meaty flavor. Made with beef, pork and chicken, tame the giant meat-lover in you with 6 grams of protein in each serving. Each spicy snack stick comes individually wrapped in a 24-count box, so you can enjoy a savory snack anywhere you want. And then there's the heat. When we say Hot AF, we mean it. So, go ahead, and fill your kitchen pantry with this spicy treat. We dare you. Don’t wait 'til that gigantic hunger comes calling; snap into a Giant Slim Jim Meat Stick and snack big.",
+	30.00,
+	5
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Ring Battery Doorbell Plus | Head-to-Toe HD+ Video, motion detection & alerts, and Two-Way Talk (2023 release)",
+	"See more of who’s there – Battery Doorbell Plus with Head-to-Toe HD+ Video gives you an expanded field of view so you can see more of who’s at your door.",
+	30.00,
+	5
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Nutricost Caffeine Pills 100mg Per Serving, 250 Capsules",
+	"No Sugar or Unhealthy Additives: Enjoy the benefits of caffeine without the unwanted sugar or other unhealthy additives commonly found in sodas or energy drinks. Convenient and Effective Capsule Formulation: Each bottle contains 250 capsules, making it easy to take your desired caffeine dose on the go.",
+	11.95,
+	30
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Hormel Chili with Beans 15 oz (Pack of 12)",
+	"Tender, savory meat and plump beans cooked with flavorful spices make HORMEL Chili everything you want chili to be – with 17 g of protein per serving! Twelve 15 oz. can of HORMEL Chili with Beans.",
+	23.90,
+	6
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"HENCKELS Statement Razor-Sharp 8-inch Slicing Knife, German Engineered Informed by 100+ Years of Mastery, Stainless Steel",
+	"Experience confidence with each cut. The Henckels CLASSIC 8 inch Chef's Knife boasts a precision, fine-edge blade that is honed for long-lasting sharpness. Chop potatoes, dice apples, mince shallots and slice salmon effortlessly - this kitchen workhorse is up for almost any task.High-quality German stainless steel strengthens this durable, Spanish-made knife. With a seamless transition from blade to handle, the knife's fully forged construction provides balance as the traditional triple-rivet handle encourages tireless cutting.Henckels makes essential kitchen tools every home chef needs. From steak knives to spatulas, every product boasts high quality at an exceptional value. Created in 1895 by Zwilling J.A. Henckels, this value-driven brand guarantees the same durability, design and ease of use for which the global company is known. Equip your kitchen with expertise with Henckels.",
+	43.60,
+	12
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Body Fortress Super Advanced Whey Protein Powder, Chocolate, Immune Support (1), Vitamins C & D Plus Zinc, 1.78 lbs (Packaging May Vary)",
+	"Body Fortress Chocolate flavored Super Advanced Whey Protein Powder is the ideal post workout protein supplement, featuring 100% premium Whey. This gluten free protein powder formula with vitamins C & D plus zinc helps support your immune system and gives you the power to train harder and rebuild lean muscle faster because tomorrow's workout starts with today's recovery.(1) Whey is the preferred protein in sports nutrition as it quickly absorbs and gets to your muscles and because of its high content of branched chain amino acids (BCAAs). BCAAs are key essential amino acids that help to activate muscle building after a hard workout.(1) (1) or ◊ - These statements have not been evaluated by the Food and Drug Administration. This product is not intended to diagnose, treat, cure or prevent any disease. Formula remains unchanged but packaging may vary. 1 scoop contains 30g of protein. 2 scoops contain 60g of protein.",
+	21.83,
+	20
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Hydro Flask Stainless Steel Wide Mouth Water Bottle with Flex Cap and Double-Wall Vacuum Insulation",
+	"Carrying Loop, Dishwasher Safe, Cold 24 Hours, Hot 12 Hours, Leakproof, Double Wall",
+	29.95,
+	30
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Goodful Hammered Carbon Steel Pow Wok Pan with Lid Cookware Set, Lid and Ergonomical Comfort Grip Handle Made from Authentic Wood, Flat Bottom is Compatible with Most Cooktops,13-Inch, Black",
+	"CLASSIC DESIGN OF AUTHENTIC PROFESSIONAL WOK: The Goodful Wok is a carbon steel pan constructed to deliver authentic results. The professionally crafted wooden handle ensures maximum control and comfort with any cooking technique. The real wooden lid helps retain moisture to help food cook faster.",
+	49.99,
+	10
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Power Strip, Nuetsa Surge Protector with 12 Outlets and 4 USB Ports, 6 Feet Flat Plug Extension Cord (1875W/15A) for for Home, Office, Dorm Essentials, 2700 Joules, ETL Listed (Black)",
+	"【Power Strip with 12AC outlets & 4 USB】- Power strip surge protector with 12AC outlets & 4 USB charging ports (5V/3.4A, 17W), 6 Feet Heavy Duty extension cord(1875W/15A), surge protector (2700 Joules) with overload protection protects against spikes and fluctuations.",
+	21.99,
+	12
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Anker Soundcore Life Dot 2 Noise Cancelling True Wireless Earbuds - Black",
+	"Model: A3922, Audio Sensitivity: 105db SPL, Frequency: 20Hz-20kHz, THD+N: <= 1%, LDS Antenna Return Loss: Typical: -15dB Limit :-12dB,Microphone:ECM Omnidirectional Mic, Audio Source: iPhone, iPad, Samsung, Blackberry, Nexus, Smartphone, Computer, PC, Notebook, MP4, MP3, and more., Proprietary Technology: AirWing, Bluetooth 5.0",
+	39.99,
+	17
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Elixir Strings - Acoustic 80/20 Bronze with NANOWEB Coating - Elixir Acoustic Guitar Strings - Baritone (.016-.070)",
+	"Top Rated Coated Acoustic Strings: Elevate your sound with our exclusive featherweight NANOWEB Coating and 80/20 bronze wrap wire for a bright, expressive tone - superior craftsmanship and durability for endless play. Baritone: 16-22-30-47-59-70",
+	20.95,
+	4
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"Zeroll, Size 20, in Silver 1020 Original Ice Cream Unique Liquid Filled Heat Conductive Handle Simple One Piece Aluminum Design Easy Release 40 Scoops per, 2-Ounce",
+	"The scoop of choice in homes and ice cream parlors since 1935, this handy tool carves out plump round scoops. Zeroll scoops create a bigger looking portion by eliminating compression, which gives you up to 20% more scoops of ice cream per gallon. Its unique handle draws on the natural warmth of your hand to ease molding the frozen treats.",
+	23.90,
+	7
+);
+INSERT INTO product (name, description, price, stock)
+VALUES (
+	"New Japanese Creative Cute Crab Pen Holder Weightlifting Crabs Penholder Bracket Storage Rack Gift Stationery",
+	"Features: A very creative pen holder, and you can use it to put other things like glasses, keys and so on. It is an office product and it is also an interesting toy and ornament. It is very cute, you can put it anywhere.",
+	8.95,
+	25
+);
