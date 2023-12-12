672364d95f189d3a55b7a478a222f6685bd9f4ff
Nevada Perry <nevadaperry@gmail.com>
2023-12-12 05:21:27 -0600

    Fix PDO/MySQL column name conflict

--- backend/resource/order.php
+++ backend/resource/order.php
@@ -6,8 +6,8 @@ function list_orders($pdo) {
 	return query_db($pdo, "
 		SELECT
 			o.id,
-			c.id,
-			c.email,
+			c.id AS customer_id,
+			c.email AS customer_email,
 			o.placed_at,
 			o.shipping_address,
 			o.name_on_card,
